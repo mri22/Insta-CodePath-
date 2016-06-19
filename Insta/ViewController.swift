d0d9f767262eb0434410a7857112fe0796cbe1da
Mazen Raafat Ibrahim <mazenibrahim@fb.com>
2016-06-18 17:26:19 -0700

    Initial Commit

---
+++ Insta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Insta
+//
+//  Created by Mazen Raafat Ibrahim on 6/18/16.
+//  Copyright © 2016 Mazen & Andres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
